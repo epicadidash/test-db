fb75f145ad7af1d8edf7c4b5034444b403711feb
epicadidash <dradwaitdash@gmail.com>
2023-03-29 00:04:04 +0530

    29.3.23

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,9 @@ enum Float {
     BigDecimal(f64),
     NDecimal(f32)
 }
+enum Tye<T>{
+Type(T)
+}
 enum Type{
 Integer(Int),
 Decimal(Float),
@@ -32,44 +35,7 @@ fn print_prompt(){
 fn inputin(s:&mut String){
     io::stdin().read_line(s).expect("failed to readline");
 }
-fn commands(star:&String, ste:& mut HashMap<String,i32>) {
-    let w = star.unicode_words().collect::<Vec<&str>>();
-    if w.len() <= 5 {
-        if w[0].to_uppercase() == "INSERT" {
-            let k:String = w[1].to_string();
-            let v:i32 = w[2].to_string().parse().unwrap();
-            ste.insert(k, v);
-            println!("Inserted")
-        }
-        else if w[0].to_uppercase() == "FIND" {
-            let t:String = w[1].to_string();
-           let v =ste.get(&t);
-           let mut k =0;
-           match v {
-              Some(i) => k=*i,
-              _ => (),
-           }
-            println!("Key:{t} , Value:{k}")
-        }
-        else if w[0].to_uppercase() == "UPDATE" {
 
-        }
-        else if w[0].to_uppercase() == "DELETE" {
-            let k:String = w[1].to_string();
-            ste.remove(&k);
-            println!("Deleted")
-        }
-        else if w[0].to_uppercase() == "SHOW"{
-            for (key, val) in ste.iter() {
-                println!("key: {key} val: {val}");
-            }
-        }
-        else {
-            println!("Wrong Command")
-        }
-    }
-   
-}
 fn chekforcommands(hei:&String,ste:& mut  HashMap<String,i32>){
     let t:Option<char> = Some(hei.chars().nth(0).unwrap());
     let he = hei;
